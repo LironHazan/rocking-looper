160af9e7379cecdefc6a6f9a710d5828f4e83ae1
lironh <lironenk@gmail.com>
2020-01-15 09:37:06 +0200

    feat(all) stating point

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,78 @@
-import { AudioComponent } from './audio/audio.component';
-import { Recorder } from './recorder/recorder';
-import { LooperUserInteractionLayer } from './ui/LooperUserInteractionLayer';
+// state machine:
+// start --> click | white
+// rec --> click | red
+// play --> click | green
+// stop --> click | blue
+// play
+// any action but start --> double click
+// start
 
-class Main {
-    static recorderInstance: Recorder = null;
 
-    static clearRecorderInstance() {
-        Main.recorderInstance = null;
-    }
-}
+const addTrack = function(blob: Blob) {
+    // Init Selectors
+    const first: HTMLButtonElement = document.querySelector('.first');
+    const second: HTMLButtonElement = document.querySelector('.second');
+    const third: HTMLButtonElement = document.querySelector('.third');
+
+
+    first.onclick = function() {
+        // templatePlay.disabled = false;
+        // templatePause.disabled = true;
+        // templateAudio.pause();
+
+        const src = URL.createObjectURL(blob);
+        const playable = new Audio(src);
+        playable.loop =  true;
+        playable.play();
+    };
+};
 
 const main = async() => {
+    // Streaming audio:
+
     // Getting permission status.
-    await navigator.permissions.query({name: 'microphone'});
+   await navigator.permissions.query({name: 'microphone'});
 
-    const playerWorker = new Worker('./media-player-worker.ts');
-    const audioInstance = new AudioComponent();
-    const stream = await audioInstance.getlineInSource();
-    const audioChunks: any[] = [];
+    const context = new AudioContext();
+    if (context.state === 'suspended') {
+        await context.resume();
+    }
 
-            Main.recorderInstance = new Recorder(stream);
-
-        Main.recorderInstance.recorder.
-        addEventListener('dataavailable', (event: any) => {
-            audioChunks.push(event.data);
+    const stream = await navigator.mediaDevices
+        .getUserMedia({
+            audio: {
+                echoCancellation: false,
+                autoGainControl: false,
+                noiseSuppression: false,
+                latency: 0
+            }
         });
+    const lineInSource = context.createMediaStreamSource(stream);
+    lineInSource.connect(context.destination);
 
-    // View Layer CTRL
-    LooperUserInteractionLayer.setupLooperUserInteractions(Main.recorderInstance, audioChunks, playerWorker);
+    // Recording streamed audio!
+    let mediaRecorder = new MediaRecorder(stream);
+    let chunks: any[] = [];
 
-    playerWorker.onmessage =  (e) => {
-        console.log('Message received from worker');
-        console.log(e);
+    let startRecord = () => {
+        mediaRecorder.start();
+    };
+    let stopRecord = () => {
+        mediaRecorder.stop();
     };
 
+    mediaRecorder.ondataavailable = <T extends { data: any }>(event: T) => {
+        chunks.push(event.data);
+    };
+
+    // Playing recorded stream
+    mediaRecorder.onstop = (event) => {
+        let blob = new Blob(chunks, {
+            'type' : 'audio/ogg; codecs=opus',
+        });
+        chunks = [];
+        addTrack(blob);
+    };
 };
 
 main()
