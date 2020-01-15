52bce37f6726d92f543b41f4bc916dd14afc49ff
lironh <lironenk@gmail.com>
2020-01-15 20:54:32 +0200

    :sparkle: fixing state machine

--- src/main.ts
+++ src/main.ts
@@ -15,39 +15,57 @@ enum State {
 }
 
 let btnState: State = State.start;
+let blob: Blob;
+let stream;
+let mediaRecorder: MediaRecorder;
 
-const addTrack = function(blob: Blob) {
-    // Init Selectors
-    const first: HTMLButtonElement = document.querySelector('.first');
-    const second: HTMLButtonElement = document.querySelector('.second');
-    const third: HTMLButtonElement = document.querySelector('.third');
+const handleTrack = function() {
+    // switch case
 
+    if (btnState === State.start) {
 
-    first.onclick = function() {
-        // templatePlay.disabled = false;
-        // templatePause.disabled = true;
-        // templateAudio.pause();
+        mediaRecorder.start();
+    }
+
+    if (btnState === State.rec) {
+
+        mediaRecorder.stop();
 
         const src = URL.createObjectURL(blob);
         const playable = new Audio(src);
         playable.loop =  true;
         playable.play();
         btnState = State.play;
-    };
+    }
+    else if (btnState === State.play) {
+
+    }
+};
+
+const attachClickListeners = (elements: HTMLButtonElement[]) => {
+    for (const el of elements) {
+        el.onclick = () => {
+            handleTrack();
+        };
+    }
 };
 
 const main = async() => {
-    // Streaming audio:
+
+    const first: HTMLButtonElement = document.querySelector('.first');
+    const second: HTMLButtonElement = document.querySelector('.second');
+    const third: HTMLButtonElement = document.querySelector('.third');
 
     // Getting permission status.
    await navigator.permissions.query({name: 'microphone'});
 
+    // Streaming audio:
     const context = new AudioContext();
     if (context.state === 'suspended') {
         await context.resume();
     }
 
-    const stream = await navigator.mediaDevices
+    stream = await navigator.mediaDevices
         .getUserMedia({
             audio: {
                 echoCancellation: false,
@@ -60,48 +78,20 @@ const main = async() => {
     lineInSource.connect(context.destination);
 
     // Recording streamed audio!
-    let mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder = new MediaRecorder(stream);
     let chunks: any[] = [];
-
-    let startRecord = () => {
-        mediaRecorder.start();
-    };
-    let stopRecord = () => {
-        mediaRecorder.stop();
-    };
 
     mediaRecorder.ondataavailable = <T extends { data: any }>(event: T) => {
         chunks.push(event.data);
     };
 
-    // Playing recorded stream
+    attachClickListeners([first, second, third]);
+
     mediaRecorder.onstop = (event) => {
-        let blob = new Blob(chunks, {
+        blob = new Blob(chunks, {
             'type' : 'audio/ogg; codecs=opus',
         });
         chunks = [];
-
-        // todo: a generic fn to init all actions
-        const first: HTMLButtonElement = document.querySelector('.first');
-        const second: HTMLButtonElement = document.querySelector('.second');
-        const third: HTMLButtonElement = document.querySelector('.third');
-
-
-        first.onclick = () => {
-            // switch case
-            // play
-            if (btnState === State.rec) {
-                const src = URL.createObjectURL(blob);
-                const playable = new Audio(src);
-                playable.loop =  true;
-                playable.play();
-                btnState = State.play;
-            }
-            else if (btnState === State.play) {
-
-            }
-
-        };
     };
 };
 
