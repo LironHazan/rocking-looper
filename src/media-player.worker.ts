836665e5f270589be0f75afc6af0aec6385bda7d
lironh <lironenk@gmail.com>
2020-02-14 14:09:21 +0200

    :sparkle: moved view from logic

--- src/media-player.worker.ts
+++
@@ -1,8 +0,0 @@
-
-onmessage = function(e){
-    console.log('Worker: Message received from main script');
-
-    postMessage('start playing', '');
-    console.log('Do something', e);
-    postMessage('done', '');
-};
